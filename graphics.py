ff99acc5933cabda2fb91035cb235c1eb65ad4af
gleb_belskij <102985700+bleb1@users.noreply.github.com>
2023-12-18 16:26:21 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,1 +1,37 @@
+import json
+from math import atan, cos, sqrt
+from matplotlib import pyplot as plt
+from numpy import arange
+R_earth = 6371000
 
+plt.style.use('grayscale')
+
+with open('data.txt', 'r') as fr:   # Читаем данные с прошлых этапов
+    PastValues = json.load(fr)
+
+def Correction(x, y):
+    h = R_earth * (1 / cos(atan(x / R_earth)) - 1)
+    return y + h
+
+x = [Correction(x[-1][0], x[-1][1]) for x in PastValues]
+y = [x[-1][0] for x in PastValues]
+a = [sqrt(a[0][1] ** 2 + a[0][0] ** 2) for a in PastValues]
+v = [sqrt(v[1][1] ** 2 + v[1][0] ** 2) for v in PastValues]
+
+plt.rcParams['font.size'] = 10
+
+t = list(arange(0, 527.81, 0.01))
+
+plt.subplot(2, 1, 1)
+plt.plot(t, a)
+plt.title("Ускорение(t)")
+
+plt.subplot(2, 2, 3)
+plt.plot(t, y)
+plt.title("Высота(t)")
+
+plt.subplot(2, 2, 4)
+plt.plot(t, v)
+plt.title("Скорость(t)")
+
+plt.show()
